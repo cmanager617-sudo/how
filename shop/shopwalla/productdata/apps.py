14b690cf7a81ca38296cc19327cac4384a7bc07b
cmanager617-sudo <cmanager617@gmail.com>
2025-08-21 12:21:53 -0700

    Delete shop/shopwalla directory

--- shop/shopwalla/productdata/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductdataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'productdata'
--- shop/shopwalla/userdata/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from userdata.models import USERDATA
-# Register your models here.
-class SHOWADMIN(admin.ModelAdmin):
-    list_display=('name','phone','address','amount','country','state','pincode','city','idno')
-
-admin.site.register(USERDATA,SHOWADMIN)
